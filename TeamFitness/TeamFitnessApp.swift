6db6592c990bb629b963be942998de921ae4a4d9
anmehta26 <anmehta26@gmail.com>
2022-07-10 09:53:58 -0500

    Initial Commit

---
+++ TeamFitness/TeamFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamFitnessApp.swift
+//  TeamFitness
+//
+//  Created by Ashu Mehta on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TeamFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
